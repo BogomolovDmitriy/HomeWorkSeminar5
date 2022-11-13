3f60079d7780d12fa3d039bf0f257807dc7c8891
Dmitriy <dimgod80@yandex.ru>
2022-11-13 12:54:35 +0800

    add Exercise34

---
+++ Exercise34/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 34:
+// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] massive = GetArray(6);
+PrintArray(massive);
+
+int[] GetArray(int length, int minValue = 100, int maxValue = 999)
+{
+    var rnd = new Random();
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
