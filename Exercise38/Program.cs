f22cdd1a37484f36c5fc6a0d57b19a5f4b82df0a
Dmitriy <dimgod80@yandex.ru>
2022-11-13 15:28:00 +0800

    add redone ex38

--- Exercise38/Program.cs
+++ Exercise38/Program.cs
@@ -2,24 +2,24 @@
 // Задайте массив вещественных чисел. 
 // Найдите разницу между максимальным и минимальным элементов массива.
 
-int[] massive = GetArray(10);
+double[] massive = GetArray(7);
 Console.Write("Созданный массив: ");
 PrintArray(massive);
 Console.WriteLine("Разница между максимальным и минимальным элементами массива: " 
                     + DifferenceMinMaxItemOnArray(massive));
 
-int[] GetArray(int length, int minValue = 0, int maxValue = 100)
+double[] GetArray(int length, int maxValue = 100)
 {
-    int[] array = new int[length];
-    var rnd = new Random();
+    double[] array = new double[length];
+    // var rnd = new Random();
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = rnd.Next(minValue, maxValue + 1);
+        array[i] = new Random().NextDouble() * maxValue;
     }
     return array;
 }
 
-void PrintArray(int[] array)
+void PrintArray(double[] array)
 {
     foreach (var item in array)
     {
@@ -28,10 +28,10 @@ void PrintArray(int[] array)
     Console.WriteLine();
 }
 
-int DifferenceMinMaxItemOnArray(int[] array)
+double DifferenceMinMaxItemOnArray(double[] array)
 {
-    int max = array[1];
-    int min = array[0];
+    double max = array[1];
+    double min = array[0];
 
     if (array[0] > array[1])
     {
